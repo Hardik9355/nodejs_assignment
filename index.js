ef7e94ecab75fff9ccbeae7de36289e8e1503247
HardikGRG <Hardik Garg>
2023-01-31 04:01:19 -0500

    First commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express =  require("express")
 const app = express();
 app.use(express.json({extended : false }));
 app.get("/", (req,res) => res.send("Server running"));
-const PORT  = process.env.PORT || 8000;
+app.post("/",(req,res)  => res.send("Server up"));
+const PORT  = process.env.PORT || 5001;
 app.listen(PORT,()=> {
     console.log("Hello! How are you ")
 });
